26e654c7bfc0aabd18c1dc7681cea4da5fbf05cf
1165244768 <1165244768@qq.com>
2021-12-11 16:30:46 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,8 +18,14 @@ class UserController extends Controller
         return view('user/show',compact('user'));
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name'     => 'required|unique:users|max:50',
+            'email'    => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        return;
     }
 }
