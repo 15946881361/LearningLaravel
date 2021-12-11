f30bf4aa1245c7238d73ce0356bf71fd6d02fe4a
1165244768 <1165244768@qq.com>
2021-12-11 17:33:38 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,9 +13,11 @@
     @include('layouts._header')
 
   <div class="container">
-    @yield('content')
-
-    @include('layouts._footer')
+    <div class="offset-md-1 col-md-10">
+      @include('layouts._message')
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </div>
 
 
