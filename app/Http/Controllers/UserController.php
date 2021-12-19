b6097ee5eab9e5a1ea5f32b04c9574ccf35fbde6
1165244768 <1165244768@qq.com>
2021-12-19 17:54:31 +0800

    线上环境发送邮箱

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
 
         $this->middleware('throttle:10,60',[
             'only'   => ['store']
-        ])
+        ]);
     }
 
     public function index()
@@ -102,13 +102,13 @@ class UserController extends Controller
     {
         $view = 'email.confirm';
         $data = compact('user');
-        $from = 'CaiCai@qq.com';
-        $name = 'CaiCai';
+        //$from = 'CaiCai@qq.com';
+        //$name = 'CaiCai';
         $to = $user->email;
         $subject = '感谢注册 Weibo 应用！请确认您的邮箱.';
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
