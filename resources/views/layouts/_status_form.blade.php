54d74958247a58cb1f0478b16f469652f3e1c4bf
1165244768 <1165244768@qq.com>
2021-12-19 22:08:08 +0800

    首页微博列表

--- resources/views/layouts/_status_form.blade.php
+++ resources/views/layouts/_status_form.blade.php
@@ -2,7 +2,7 @@
   @include('layouts._error')
   {{ csrf_field() }}
   <textarea class="form-control" rows="3" placeholder="聊聊新鲜事儿..." name="content">{{ old('content') }}</textarea>
-  <div class="text-right">
+  <div class="text-right" style="text-align:right">
       <button type="submit" class="btn btn-primary mt-3">发布</button>
   </div>
 </form>
